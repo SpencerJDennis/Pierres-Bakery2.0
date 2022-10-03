fc9cc9decf7de61ad5095680370ed4994dd08e44
Spencer Dennis <sjdennis2113@gmail.com>
2022-10-03 06:16:16 -0700

    wrote code for Pastry still have modified error

--- pierres-bakery.solution/pierresbakery/Models/Pastry.cs
+++ pierres-bakery.solution/pierresbakery/Models/Pastry.cs
@@ -2,6 +2,42 @@ namespace PierresBakery.Models
 {
   public class Pastry
   {
-   
+    private int PastryQuantity { get; set; }
+
+    public Pastry(int pastryQuantity)
+    {
+      PastryQuantity = pastryQuantity
+    }
+
+    public int PastryCost()
+    {
+     
+      int pastryPrice = 2;
+      int pastryDiscountPrice = 1;
+
+      if(PastryQuantity < 3)
+      {
+      int orderPrice = pastryPrice * PastryQuantity;
+      return orderPrice;
+      }
+      else if (PastryQuantity >= 3)
+      {
+      int discountPastry = PastryQuantity / 3;
+
+      int normalPastryPrice = PastryQuantity - discountPastry;
+      
+      int discount = discountPastry * pastryDiscountPrice;
+      
+      int normalPastry = normalPastryPrice * pastryPrice;
+      
+      int orderTotal = discount + normalPastry;
+      
+      return orderTotal;
+      }
+      else
+      {
+      return 0;
+      }
+    }
   }
-}
+} 
